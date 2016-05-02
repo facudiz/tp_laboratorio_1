202b166c990069570af1db32979ad98930c924e7
facudiz <facudiz13@gmail.com>
2016-05-02 16:59:19 -0300

    Delete funciones2.h

--- funciones2.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-
-int getInt(int input,char message[], char eMessage[], int lowLimit);
-int getChar(char* input,char message[], char eMessage[], char lowLimit, char hiLimit);
-int getString(char* input,char message[], char eMessage[], int lowLimit, int hiLimit);
